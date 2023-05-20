743325d493cdb16eab1c7bdd161441736310a420
David Tealdi <tealdidavid12@gmail.com>
2023-05-20 11:05:46 -0300

    cambio en el paginado

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -4,17 +4,21 @@ import Character from "./Character"
 function NavPage( { page, setPage } ) {
     return (
         <header className="d-flex justify-content-between align-items-center">
-            <p>Page: {page}</p>
-            <button className="btn btn-primary btn-sm"
-            onClick={() => setPage(page - 1)}
-            >
-                volver Page {page - 1 || page + 1 }
+            
+            <button className="btn btn-primary btn-sm" onClick={() => setPage(page - 1)} >
+                
+                volver Paguina 
+            
             </button>
-            <button className="btn btn-primary btn-sm"
-            onClick={() => setPage(page + 1)}
-            >
-                Page siguiente {page + 1}
+            
+            <p>Paguina: {page}</p>
+            
+            <button className="btn btn-primary btn-sm" onClick={() => setPage(page + 1)} >
+                
+                Paguina siguiente 
+            
             </button>
+        
         </header>
     )
 }
